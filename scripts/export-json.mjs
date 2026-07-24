626afd6d1023c3cf52d069738d14e806961b0d60
hyvinvointivaltio <harrikayhko@outlook.com>
2026-07-24 19:26:06 +0300

    Add files via upload

--- scripts/export-json.mjs
+++ scripts/export-json.mjs
@@ -1,14 +1,1 @@
-await writeFile(
-  "suomen_murrosvaiheet_syvennetty.json",
-  JSON.stringify(murrosExport, null, 2) + "\n"
-);
-
-await writeFile(
-  "artikkelit.json",
-  JSON.stringify(artikkelitExport, null, 2) + "\n"
-);
-
-await writeFile(
-  "yhdistys.json",
-  JSON.stringify(yhdistysExport, null, 2) + "\n"
-);
+Unsupported Media Type
